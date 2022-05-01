6c60c086a56d72ccc9c9d617dd35dad064257e6d
justintroy <xermasterz@gmail.com>
2022-05-01 23:26:02 +0800

    Login and Registration System

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,14 +1,23 @@
 from re import template
 from django.urls import path
-from . import views
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
+
+from . import views
+from .forms import UserLoginForm
 
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
-    path('login/', views.login, name='login'),
+    path('home/', views.dashboard, name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='includes/login.html',
+                                                authentication_form=UserLoginForm), name='login'),
+    path('logout/', views.logout_user, name='logout'),
     path('register/', views.register, name='register'),
     path('register/details', views.register_details, name='details',),
     path('register/account-type', views.account_type, name='account_type'),
     path('register/success', views.register_success, name='register_success'),
-]
+    path('activate/<slug:uidb64>/<slug:token>)/',
+         views.activate, name='activate'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- servicify_web/settings.py
+++ servicify_web/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'dashboard.views.avatar',
             ],
         },
     },
@@ -136,7 +137,15 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' PROD
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # DEV
+
+LOGIN_REDIRECT_URL = '/'
